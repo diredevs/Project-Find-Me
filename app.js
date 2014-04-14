1145a5519d4d801cd4f56a446ed7a51821f85a3b
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-04-13 23:57:25 -0300

    Add POST geoPosition route #1

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
 var db = require('db.js');
+var geoPosition = require('app/models/geo_position.js');
 
 app.get('/', function(req, res){
-    res.send('hello world');
+  res.send('hello world');
+});
+
+app.post('/geo-position', function(req, res) {
+  var lat = req.query.lat,
+      lng = req.query.lng;
+  geoPosition.create({
+    lat: lat,
+    lng: lng
+  }, function(err, geoPosition) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(geoPosition);
+    }
+  });
 });
 
 app.listen(3000);
