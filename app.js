d777936b6df848a041f7b248bad9f591ff44b216
Bernardo Gurgel <bernardogfilho@gmail.com>
2014-04-16 14:06:36 -0300

    Use body parser middleware

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var db = require('./db.js');
 var geoPosition = require('./app/models/geo_position.js');
+
+app.use(bodyParser());
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
 app.post('/geo-position', function(req, res) {
-  var lat = req.query.lat,
-      lng = req.query.lng;
   geoPosition.create({
-    lat: lat,
-    lng: lng
+    lat: req.param('lat'),
+    lng: req.param('lng')
   }, function(err, geoPosition) {
     if (err) {
       res.send(err);
